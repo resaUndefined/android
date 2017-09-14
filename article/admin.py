21ceec9f6c4e5358a9555e09f1f0de5cde41b8f3
resaUndefined <resamuhammad96@gmail.com>
2017-09-14 20:40:36 +0700

    push kuy

--- article/admin.py
+++ article/admin.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from article.models import Category,Post
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','category','created_on','last_modified','image')
+	list_filter = ('category','created_on')
+	search_fields = ('title','content')
+	prepopulated_fields = {'slug' : ('title',)}
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
